a26110f496b038a57c7fe5df0f2d3e3ea2103839
koushikgarg11 <145890848+koushikgarg11@users.noreply.github.com>
2026-03-05 17:06:34 +0530

    Update ai_engine.py

--- modules/ai_engine.py
+++ modules/ai_engine.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def generate_ai_summary(df):
 
@@ -17,7 +17,7 @@ def generate_ai_summary(df):
 
     Provide:
     - Key trends
-    - Interesting patterns
+    - Patterns
     - Business insights
     """
 
