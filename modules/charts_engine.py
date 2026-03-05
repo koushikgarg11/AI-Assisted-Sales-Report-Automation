1f8871b2ad3ca143efb0a60858ee51cab10fcf98
koushikgarg11 <145890848+koushikgarg11@users.noreply.github.com>
2026-03-05 13:23:32 +0530

    Create charts_engine.py

---
+++ modules/charts_engine.py
@@ -0,0 +1,26 @@
+import plotly.express as px
+
+def generate_chart(df, x, y, chart_type):
+
+    if chart_type == "bar":
+        fig = px.bar(df, x=x, y=y)
+
+    elif chart_type == "line":
+        fig = px.line(df, x=x, y=y)
+
+    elif chart_type == "scatter":
+        fig = px.scatter(df, x=x, y=y)
+
+    elif chart_type == "box":
+        fig = px.box(df, x=x, y=y)
+
+    elif chart_type == "histogram":
+        fig = px.histogram(df, x=y)
+
+    elif chart_type == "pie":
+        fig = px.pie(df, names=x, values=y)
+
+    else:
+        fig = px.bar(df, x=x, y=y)
+
+    return fig
